0b62b637c6eed27e3631f0d657457872e67f0056
AshotVantsyan <ashotvantsyan@gmail.com>
2021-10-16 19:40:28 +0400

    Improved logging handling.

--- src/main.py
+++ src/main.py
@@ -13,21 +13,23 @@ from typing import List
 
 from pyramid_slide_down import longest_slide_down
 
+logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO, name="main")
+logger = logging.getLogger("slide-down")
 
 def check_asset_validity(asset: str) -> None:
     """
     Check if asset is a valid file in JSON format.
     """
     if not os.path.isfile(asset):
-        logging.error("'%s' doesn't exist", asset)
+        logger.error("'%s' doesn't exist", asset)
         sys.exit(2)
     try:
         with open(asset, 'r', encoding='utf-8') as file:
             json.load(file)
     except ValueError as error:
-        logging.error("Invalid JSON is provided.")
-        logging.error("Asset: %s", asset)
-        logging.error(error)
+        logger.error("Invalid JSON is provided.")
+        logger.error("Asset: %s", asset)
+        logger.error(error)
         sys.exit(3)
 
 
@@ -43,9 +45,9 @@ def get_assets(arguments: List[str]) -> List[str]:
     elif os.path.isdir("assets"):
         assets = [os.path.join("assets", asset) for asset in os.listdir("assets")]
     else:
-        logging.error("'assets' directory hasn't not found")
+        logger.error("'assets' directory hasn't not found")
         sys.exit(1)
-    logging.info("Selected asset(s): %s", ", ".join(assets))
+    logger.info("Selected asset(s): %s", ", ".join(assets))
     return assets
 
 
@@ -58,7 +60,7 @@ def get_pyramid_from_asset(asset: str) -> List[List[int]]:
         pyramid: List[List[int]] = json.load(file)
     for count, row in enumerate(pyramid, start=1):
         if count != len(row):
-            logging.error("Invalid pyramid. Expected %s elements "
+            logger.error("Invalid pyramid. Expected %s elements "
                           "at %s row, got %s", count, count, len(row))
             sys.exit(4)
     return pyramid
@@ -68,12 +70,11 @@ def main() -> None:
     """
     Entrypoint function for the project.
     """
-    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
     assets = get_assets(sys.argv[1:])
     pyramids = [get_pyramid_from_asset(asset) for asset in assets]
     for i, pyramid in enumerate(pyramids):
         max_path_sum = longest_slide_down(pyramid)
-        logging.info("Asset: %s, Longest Slide Down: %s", assets[i], max_path_sum)
+        logger.info("Asset: %s, Longest Slide Down: %s", assets[i], max_path_sum)
 
 if __name__ == "__main__":
     main()
